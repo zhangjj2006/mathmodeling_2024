877ad131402e9594f0b63171915c65727382d8c2
zhangjj <15985182+zhangjj2006@user.noreply.gitee.com>
2025-09-05 10:06:43 +0800

    tupian

--- python_code/C(1).py
+++ python_code/C(1).py
@@ -1,16 +1,34 @@
 import pandas as pd
+import numpy as np
+from scipy import stats
 
-# 读取 Excel 文件
 df = pd.read_excel("python_code//附件.xlsx")
 
-# 统计每个编号出现的次数
 counts = df["孕妇代码"].value_counts()
 
-# 筛选出出现次数大于 3 次的编号
 valid_ids = counts[counts > 3].index
 
-# 从原始 DataFrame 中剔除出现次数不高于 3 次的编号对应的数据
 filtered_df = df[df["孕妇代码"].isin(valid_ids)]
+filtered_df = filtered_df[['孕妇代码', '检测孕周','孕妇BMI', 'Y染色体浓度']]
+filtered_df.to_excel("python_code//附件_剔除后数据.xlsx", index=False)
 
-# 将结果保存为新的 Excel 文件
-filtered_df.to_excel("文件//附件_剔除后数据.xlsx", index=False)
+def calculate_regression_coefficients(group):
+    """
+    计算线性回归的斜率和截距
+    """
+
+    x = group['孕周']
+    y = group['头围']
+    
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+    
+    return pd.Series({
+        '斜率': slope,
+        '截距': intercept
+    })
+
+regression_coefficients = filtered_df.groupby('孕妇代码').apply(calculate_regression_coefficients)
+
+result_df = filtered_df.merge(regression_coefficients, on='孕妇代码')
+
+result_df.to_excel("python_code//附件_剔除后数据_含回归系数.xlsx", index=False)
